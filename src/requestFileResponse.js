622f2be57ea830a5ff0c146445b860f4c4d12328
Luiz Augusto Schneider da Silva <luizz.augustoll@hotmail.com>
2022-03-09 13:08:41 -0300

    improving requestFileResponse.js code

--- src/requestFileResponse.js
+++ src/requestFileResponse.js
@@ -2,13 +2,13 @@
  * 
  * @param {string} path
  * @param {string} contentType
- * @param {(text: string) => Promise<string>|undefined} transformTextBeforeRespond
+ * @param {(text: string|Promise<string>) => string|Promise<string>} transformTextBeforeRespond
  */
-export async function requestFileResponse(path, contentType = "text/plain", transformTextBeforeRespond = async (text) => await text) {
+export async function requestFileResponse(path, contentType = "text/plain", transformTextBeforeRespond = (text) => text) {
   try {
-    const file = await Deno.readTextFile(path)
+    const file = Deno.readTextFile(path)
     return new Response(await transformTextBeforeRespond(file), { status: 200, headers: { "content-type": contentType } })
   }
   catch(e) { console.error(e) }
-  return await notFoundResponse()
+  return await new Response(undefined, { status: 404 })
 }
